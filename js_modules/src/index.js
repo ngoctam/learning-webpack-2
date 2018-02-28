4c3203d64aa63063a26f580ec260fe0c5bb6233c
ngoctam <nguyenngoctamvn@gmail.com>
2018-02-28 08:51:33 +0700

    Codesplitting with Webpack and System.import

--- js_modules/src/index.js
+++ js_modules/src/index.js
@@ -2,8 +2,18 @@
 // const sum = require('./sum');
 
 // ES2015
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
 
-const total = sum(5, 10);
-console.log(total);
+// const total = sum(5, 10);
+// console.log(total);
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.onclick = () => {
+    System.import('./image_viewer_2').then(module => {
+        console.log(module);
+    });
+};
+
+document.body.appendChild(button);
