89126c427954ecccc8baf1a6e301107825b83672
ngoctam <nguyenngoctamvn@gmail.com>
2018-02-28 21:35:44 +0700

    Use Vendor, Code Splitting with webpack.optimize.CommonsChunkPlugin and html-webpack-plugin

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -1,11 +1,21 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  "faker", "lodash", 
+  "react", "react-dom", "react-input-range", "react-redux", "react-router",
+  "redux", "redux-form", "redux-thunk"
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: { // Webpack 2, loaders -> Webpack 2
     rules: [
@@ -19,5 +29,13 @@ module.exports = {
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
